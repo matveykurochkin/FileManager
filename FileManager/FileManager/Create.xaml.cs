dc9b1651137b628b854c056969dedc9610687ebf
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-03-08 13:00:33 +0300

    add form for creation files and folders

---
+++ FileManager/FileManager/Create.xaml.cs
@@ -0,0 +1,40 @@
+﻿using FileManager.Include;
+using System.Windows;
+
+namespace FileManager
+{
+    public partial class Create : Window
+    {
+        public Create()
+        {
+            InitializeComponent();
+        }
+
+        private void AddFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameOfFileOrFolder.Text != "")
+            {
+                Function.AddFolder(Function.pathOnTCWindow, NameOfFileOrFolder.Text);
+                Close();
+            }
+        }
+
+        private void AddFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameOfFileOrFolder.Text != "")
+            {
+                Function.AddFile(Function.pathOnTCWindow, NameOfFileOrFolder.Text);
+                Close();
+            }
+        }
+
+        private void AddWordDocumentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameOfFileOrFolder.Text != "")
+            {
+                Function.AddFile(Function.pathOnTCWindow, NameOfFileOrFolder.Text,Function.MSWord);
+                Close();
+            }
+        }
+    }
+}
