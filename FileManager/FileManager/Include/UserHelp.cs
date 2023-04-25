e24a07e14dca45a13976ec827b83828558251df8
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-04-25 16:32:33 +0300

    create class for help information

---
+++ FileManager/FileManager/Include/UserHelp.cs
@@ -0,0 +1,84 @@
+﻿namespace FileManager.Include
+{
+    internal static class UserHelp
+    {
+        public static readonly string help = $"\t\t\t\t\tРуководство пользователя Файлового менеджера. v0.7.2" +
+            "\n\t\t\t\t\t\tРусская версия" +
+            "\n\tВсе кнопки на интерфейсе заменены соответствующими значками, при наведении на кнопку, будет написано ее название." +
+            "\nСоздание папок и файлов: " +
+            "\n\tДля создания папки по умолчанию требуется перейти в нужный каталог и нажать кнопку New Folder!" +
+            "\n\tДля создания файла по умолчанию требуется перейти в нужный каталог и нажать кнопку New File!" +
+            "\n\tДля создания папки или файла с собственным именем нажать ПКМ и в окне ввести название!" +
+            "\nПо умолчанию или при нажатии Enter создается папка в текущем каталоге!" +
+            "\nКопирование папок:" +
+            "\n\tДля копирования папки необходимо в левом окне выбрать папку, необходимую для копирования, а в правом окне папку, в которую нужно скопировать данные, после нажать кнопку Copy!" +
+            "\nТакже можно воспользоваться быстрой командой, нажав кнопку F7, или нажать на соответствующую кнопку на панели быстрых команд!" +
+            "\nУдаление папок:" +
+            "\n\tДля копирования только файлов:" +
+            "\nЧтобы скопировать только файлы из текущей папки в папку, необходимую для копирования, нужно нажать на кнопку Copy Files! (в левом окне папка откуда копировать, в правом куда)" +
+            "\n\tДля удаления папок необходимо зайти в папку которую нужно удалить и нажать кнопку Remove, после чего папка и все ее содержимое безвозвратно удалится!" +
+            "\nОбновление данных:" +
+            "\n\tЕсли файлы были добавлены через другое приложение, то следует нажать кнопку Update!" +
+            "\nТакже можно воспользоваться быстрой командой, нажав кнопку F5, или нажать на соответствующую кнопку на панели быстрых команд!" +
+            "\nСброс всех окон и их данных:" +
+            "\n\tЧтобы сбросить все окна до состояния по умолчанию, необходимо нажать кнопку Reset!" +
+            "\nТакже можно воспользоваться быстрой командой, нажав кнопку F6, или нажать на соответствующую кнопку на панели быстрых команд!" +
+            "\n\tВыход и перезапуск приложения:" +
+            "\nДля перезапуска приложения можно воспользоваться быстрой командой, нажав кнопку F3, или нажать на соответствующую кнопку на панели быстрых команд!" +
+            "\nДля выхода можно воспользоваться быстрой командой, нажав кнопку Alt + F4, или нажать на соответствующую кнопку на панели быстрых команд!" +
+            "\nКопирование пути:" +
+            "\n\tДля того, чтобы скопировать путь необходимо перейти в нужный каталог и нажать кнопку Copy в зависимости от того, в каком окне вы находитесь!" +
+            "\nДля перехода по скопированному пути:" +
+            "\n\tДля того, чтобы перейти по скопированному пути, нужно вставить путь в одно из полей, где располагается путь и нажать Enter или на кнопку Next!" +
+            "\nОткрытие Блокнота:" +
+            "\n\tДля того, чтобы откыть блокнот, необходимо нажать кнопку Notepad!" +
+            "\nТакже можно воспользоваться быстрой командой, нажав кнопку F9, или нажать на соответствующую кнопку на панели быстрых команд!" +
+            "\nДля открытия CMD:" +
+            "\n\tДля того, чтобы открыть CMD, необходимо нажать кнопку CMD!" +
+            "\nТакже можно воспользоваться быстрой командой, нажав кнопку F8, или нажать на соответствующую кнопку на панели быстрых команд!" +
+            "\nФайловый менеджер предоставляет информацию о памяти текущего диска, его файловой системы и названия, также показывает скрытые папки!" +
+            "\n\tДля поиска файлов или директорий:" +
+            "\nЧтобы найти нужный файл или папку, необходимо выбрать папку в которой нужно искать этот файл или папку (нельзя искать в корне диска, потому что" +
+            "\nтам могут находится папки к которым у приложения нет доступа, что приведет к отрицательным результатам поиска), поиск требует точного названия файла (с расширением) " +
+            "\nили папки, к регистру не чувствителен!" +
+            "\n\t\t\t\t\tFile Manager User Guide. v0.7.2" +
+            "\n\t\t\t\t\t\tEnglish Version" +
+            "\n\tAll buttons on the interface have been replaced with corresponding icons. Hovering over a button will display its name." +
+            "\nCreating Folders and Files:" +
+            "\n\tTo create a default folder, go to the desired directory and click on the New Folder button!" +
+            "\n\tTo create a default file, go to the desired directory and click on the New File button!" +
+            "\n\tTo create a folder or file with a custom name, right-click and enter the desired name in the window!" +
+            "\nBy default or when Enter is pressed, a folder is created in the current directory!" +
+            "\nCopying Folders:" +
+            "\n\tTo copy a folder, select the folder to be copied in the left window, and the folder to copy to in the right window, then click on the Copy button!" +
+            "\nYou can also use the quick command by pressing F7 or clicking on the corresponding button on the quick command panel!" +
+            "\nDeleting Folders:" +
+            "\n\tTo copy only files:" +
+            "\nTo copy only files from the current folder to the folder where they are to be copied, click on the Copy Files button! (in the left window, select the folder to copy from, and in the right window, select the folder to copy to)" +
+            "\n\tTo delete a folder, go to the folder to be deleted and click on the Remove button, after which the folder and all its contents will be permanently deleted!" +
+            "\nUpdating Data:" +
+            "\n\tIf files were added through another application, click on the Update button!" +
+            "\nYou can also use the quick command by pressing F5 or clicking on the corresponding button on the quick command panel!" +
+            "\nResetting all windows and their data:" +
+            "\n\tTo reset all windows to their default state, click on the Reset button!" +
+            "\nYou can also use the quick command by pressing F6 or clicking on the corresponding button on the quick command panel!" +
+            "\nExiting and restarting the application:" +
+            "\nTo restart the application, use the quick command by pressing F3 or clicking on the corresponding button on the quick command panel!" +
+            "\nTo exit, use the quick command by pressing Alt + F4 or clicking on the corresponding button on the quick command panel!" +
+            "Copying a Path:" +
+            "\n\tTo copy a path, you need to go to the desired directory and click the Copy button depending on which window you are in!" +
+            "\nNavigating to a Copied Path:" +
+            "\n\tTo navigate to a copied path, you need to paste the path into one of the fields where the path is located and press Enter or the Next button!" +
+            "\nOpening Notepad:" +
+            "\n\tTo open Notepad, you need to click the Notepad button!" +
+            "\nYou can also use a quick command by pressing F9, or click the corresponding button on the Quick Commands panel!" +
+            "\nOpening CMD:" +
+            "\n\tTo open CMD, you need to click the CMD button!" +
+            "\nYou can also use a quick command by pressing F8, or click the corresponding button on the Quick Commands panel!" +
+            "\nThe file manager provides information about the memory of the current disk, its file system, and name. It also shows hidden folders!" +
+            "\n\tSearching for Files or Directories:" +
+            "\nTo find the necessary file or folder, you need to select the folder in which you want to search for this file or folder (you cannot search in the root of the disk because " +
+            "\nthere may be folders to which the application does not have access, which will lead to negative search results). The search requires the exact name of the file (with extension) " +
+            "\nor folder, and it is not case sensitive!";
+    }
+}
